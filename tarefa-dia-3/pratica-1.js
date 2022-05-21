1fab1e9263dd4d814ab7d344c28ef39b5c8c4b54
Lauren2022 <laurenrosalima@gmail.com>
2022-05-21 20:14:31 -0300

    Ajustando os valores das variáveis ao texto.

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -20,3 +20,12 @@ lugarDosSonhos = "Coimbra, Portugal"
 //Exibindo os valores das variáveis na tela
 
 console.log(nome, email, profissao, idade, lugarDosSonhos)
+
+//Ajustando os valores das variáveis ao texto
+
+console.log("Olá", nome,"!")
+console.log("Somos aqui da agência de turismo Viagem dos Sonhos.")
+console.log("Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para", lugarDosSonhos, ".")
+console.log("Confirma pra gente alguns dados?")
+console.log("A sua idade é", idade,", você é", profissao, "e o seu email é", email,"?")
+console.log("Ficamos no aguardo. Muito obrigado e boa viagem! :D")
