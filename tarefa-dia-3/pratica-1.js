187900dc0347b03cc6b4e4284665658174045f8f
Lauren2022 <laurenrosalima@gmail.com>
2022-05-21 18:58:48 -0300

    Alô pro mundo e declarando variáveis

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,11 @@
+console.log("Olá mundo, mano velho!")
+console.log("Meu primeiro programa do ITuring.")
+
+//Declarando as variáveis
+
+let nome
+let email
+let profissao
+let idade
+let lugarDosSonhos
+
