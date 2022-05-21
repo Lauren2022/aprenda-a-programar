a104819b8311b5870ceeb68c5d72040113d0b8fa
Lauren2022 <laurenrosalima@gmail.com>
2022-05-21 19:11:09 -0300

    Atribuindo valores às variáveis.

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -9,3 +9,11 @@ let profissao
 let idade
 let lugarDosSonhos
 
+//Atribuindo valor às vaiáveis
+
+nome = "Lauren"
+email = "laurenrosalima@gmail.com"
+profissao = "psicóloga"
+idade = 54
+lugarDosSonhos = "Coimbra, Portugal"
+
